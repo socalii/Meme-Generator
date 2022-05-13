f945c978404cc6961ec5aee659e9dd7d49885f0f
Susan Choi <2539lee@gmail.com>
2022-05-13 00:36:53 -0700

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img className='header--icon' src='/images/troll-face.png' />
+      <h2 className='header--title'>Meme Generator</h2>
+      <h4 className='header--project'>React Course - Project 3</h4>
+    </header>
+  )
+}
+
+export default Header
